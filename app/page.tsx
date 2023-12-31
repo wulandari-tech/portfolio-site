a1975c8c772fffa256dc998643a2f2d70043601a
James Graca-Jones <jamesgracajones@gmail.com>
2023-12-31 18:19:59 +0000

    Add bio text

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
 export default function Home() {
   return (
-    <main></main>
+    <main>
+      <h1>Welcome to my portfolio site!</h1>
+      <section>
+        <h2>About me...</h2>
+        <div>
+          <p>
+            My name is <strong>James Graca-Jones</strong> and I am a <strong>software developer</strong> living in <strong>London</strong>.
+          </p>
+          {/* TODO: add display photo */}
+        </div>
+      </section>
+    </main>
   )
 }
