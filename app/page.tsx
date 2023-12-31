cb33ed896b1a16d857fdd2a39dde78519f1ad2ca
James Graca-Jones <jamesgracajones@gmail.com>
2023-12-31 18:23:10 +0000

    Add headshot to bio

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image"
+
 export default function Home() {
   return (
     <main>
@@ -8,7 +10,8 @@ export default function Home() {
           <p>
             My name is <strong>James Graca-Jones</strong> and I am a <strong>software developer</strong> living in <strong>London</strong>.
           </p>
-          {/* TODO: add display photo */}
+          {/* TODO: decide which Image elements should be given the priority tag */}
+          <Image src="/headshot.jpeg" alt="" width={50/96*98} height={50} priority/>
         </div>
       </section>
     </main>
