d06dab4af625147fbf6218e7d28e9d8c93755f05
James Graca-Jones <jamesgracajones@gmail.com>
2024-01-01 20:26:39 +0000

    Outline sections of projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,14 @@
+export default function Index() {
+    return (
+      // TODO: link skills with certifications to their certifications
+      <main>
+        <h1>My Projects...</h1>
+        <section>
+            <h2>Featured Projects...</h2>
+        </section>
+        <section>
+            <h2>Further Projects...</h2>
+        </section>
+      </main>
+    )
+  }
