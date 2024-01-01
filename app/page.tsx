fd740e347d40e1dac4d634767c8dcafcb65a409e
James Graca-Jones <jamesgracajones@gmail.com>
2024-01-01 19:13:53 +0000

    Put bio elements at root of their section element

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,11 @@ export default function Home() {
       <h1>Welcome to my portfolio site!</h1>
       <section>
         <h2>About me...</h2>
-        <div>
-          <p>
-            My name is <strong>James Graca-Jones</strong> and I am a <strong>software developer</strong> living in <strong>London</strong>.
-          </p>
-          {/* TODO: decide which Image elements should be given the priority tag */}
-          <Image src="/headshot.jpeg" alt="" width={50/96*98} height={50} priority/>
-        </div>
+        <p>
+          My name is <strong>James Graca-Jones</strong> and I am a <strong>software developer</strong> living in <strong>London</strong>.
+        </p>
+        {/* TODO: decide which Image elements should be given the priority tag */}
+        <Image src="/headshot.jpeg" alt="" width={50/96*98} height={50} priority/>
       </section>
       <section>
         <h2>Education...</h2>
