f3fd10cd42c343986a0c8598b5b136b6986b3be6
James Graca-Jones <jamesgracajones@gmail.com>
2024-01-02 19:50:10 +0000

    Add TODO

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -17,17 +17,20 @@ function makeUnfeaturedProjectList(projects: Project[]): Project[] {
 }
 
 export default function Index() {
+    const featuredProjects = makeFeaturedProjectList(projects);
+    const unfeaturedProjects = makeUnfeaturedProjectList(projects);
     return (
       // TODO: link skills with certifications to their certifications
       <main>
         <h1>My Projects...</h1>
+        {/* TODO: consider rendering each list only if there are relevant projects and amending their headings accordingly depending on which lists are rendered (e.g. shouldn't say "More..." if there's only one list) */}
         <section>
             <h2>Featured...</h2>
-            <ProjectList projectList={makeFeaturedProjectList(projects)}/>
+            <ProjectList projectList={featuredProjects}/>
         </section>
         <section>
             <h2>More...</h2>
-            <ProjectList projectList={makeUnfeaturedProjectList(projects)}/>
+            <ProjectList projectList={unfeaturedProjects}/>
         </section>
       </main>
     );
