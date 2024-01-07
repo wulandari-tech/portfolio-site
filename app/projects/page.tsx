1310f3de6797c736374c2520aff515f795d8af87
James Graca-Jones <jamesgracajones@gmail.com>
2024-01-07 18:20:14 +0000

    Fix typo

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -20,7 +20,7 @@ export default function ProjectsPage() {
     const featuredProjects = makeFeaturedProjectList(projects);
     const unfeaturedProjects = makeUnfeaturedProjectList(projects);
     return (
-      // TODO: link skills with certifications to their certifications
+      // TODO: link skills to their certifications
       <main>
         <h1>My Projects...</h1>
         {/* TODO: consider rendering each list only if there are relevant projects and amending their headings accordingly depending on which lists are rendered (e.g. shouldn't say "More..." if there's only one list) */}
