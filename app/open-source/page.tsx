2c1f0cb17342115455d38bf145610fb9eced129a
James Graca-Jones <jamesgracajones@gmail.com>
2024-01-07 18:29:13 +0000

    Make static OS page

--- app/open-source/page.tsx
+++ app/open-source/page.tsx
@@ -1,7 +1,15 @@
 export default function OpenSourcePage() {
     return (
-        <div>
-            Open source page.
-        </div>
+      <main>
+        <h1>My Open Source Contributions...</h1>
+        <section>
+            <h2>Featured...</h2>
+            {/* <ProjectList projectList={featuredProjects}/> */}
+        </section>
+        <section>
+            <h2>More...</h2>
+            {/* <ProjectList projectList={unfeaturedProjects}/> */}
+        </section>
+      </main>
     );
 }
