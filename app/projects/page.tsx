2dcba9d5ed73e38766aaaee09e53593930071dee
James Graca-Jones <jamesgracajones@gmail.com>
2024-01-07 18:31:12 +0000

    Extend TODOs

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -20,10 +20,10 @@ export default function ProjectsPage() {
     const featuredProjects = makeFeaturedProjectList(projects);
     const unfeaturedProjects = makeUnfeaturedProjectList(projects);
     return (
-      // TODO: link skills to their certifications
+      // TODO: link skills to their certifications, here and in the open source page for example
       <main>
         <h1>My Projects...</h1>
-        {/* TODO: consider rendering each list only if there are relevant projects and amending their headings accordingly depending on which lists are rendered (e.g. shouldn't say "More..." if there's only one list) */}
+        {/* TODO: consider rendering each list only if there are relevant projects and amending their headings accordingly depending on which lists are rendered (e.g. shouldn't say "More..." if there's only one list), here and in the open source page for example */}
         <section>
             <h2>Featured...</h2>
             <ProjectList projectList={featuredProjects}/>
