ff61a2b89a09e794150041cc071662d0bede2d29
James Graca-Jones <jamesgracajones@gmail.com>
2024-01-09 21:39:40 +0000

    Implement typing on List

--- app/open-source/page.tsx
+++ app/open-source/page.tsx
@@ -2,19 +2,21 @@ import List from "@/components/List";
 import OpenSourceProject from "@/components/OpenSourceProject";
 import openSourceProjects from "@/data/openSourceProjects";
 import OpenSourceProjectList from "@/components/OpenSourceProjectList";
+import { OpenSourceProjectProps } from "@/types";
 
 export default function OpenSourcePage() {
-    return (
-      <main>
-        <h1>My Open Source Contributions...</h1>
-        <section>
-            <h2>Featured...</h2>
-            <List dataList={openSourceProjects.map(project => ({project, contributorUsername: 'JamesGJ5'}))} ListedComponent={OpenSourceProject}/>
-            {/* <OpenSourceProjectList projectList={openSourceProjects} contributorUsername="JamesGJ5"/> */}
-        </section>
-        <section>
-            <h2>More...</h2>
-        </section>
-      </main>
-    );
+  const dataForList: OpenSourceProjectProps[] = openSourceProjects.map(project => ({project, contributorUsername: 'JamesGJ5'}));
+  return (
+    <main>
+      <h1>My Open Source Contributions...</h1>
+      <section>
+          <h2>Featured...</h2>
+          <List dataList={dataForList} ListedComponent={OpenSourceProject}/>
+          {/* <OpenSourceProjectList projectList={openSourceProjects} contributorUsername="JamesGJ5"/> */}
+      </section>
+      <section>
+          <h2>More...</h2>
+      </section>
+    </main>
+  );
 }
--- components/List.tsx
+++ components/List.tsx
@@ -1,9 +1,15 @@
-// @ts-nocheck
 // TODO: sort out typing
+import React from "react"
 
-export default function List({ dataList, ListedComponent }) {
+interface ListProps<T extends object> {
+    dataList: T[],
+    ListedComponent: React.ComponentType<T>,
+}
+
+export default function List<T extends object>({ dataList, ListedComponent }: ListProps<T>) {
     return (
         <ol>
+            {/* TODO: use a more unique ID for key */}
             {dataList.map((data, index) => <li key={index}>
                 <ListedComponent {...data}/>
             </li>)}
