72249a5f527ee05cf744900b61f1bbf6786d2787
James Graca-Jones <jamesgracajones@gmail.com>
2024-01-10 20:18:08 +0000

    Split OS projects by whether featured and sort

--- app/open-source/page.tsx
+++ app/open-source/page.tsx
@@ -1,19 +1,22 @@
 import List from "@/components/List";
 import OpenSourceProject from "@/components/OpenSourceProject";
 import openSourceProjects from "@/data/openSourceProjects";
+import { makeFeaturedProjectList, makeUnfeaturedProjectList } from "@/utils/projectUtils";
 
 export default function OpenSourcePage() {
-  const dataForList = openSourceProjects.map(project => ({project, contributorUsername: 'JamesGJ5'}));
+  const featuredProjects = makeFeaturedProjectList(openSourceProjects);
+  const unfeaturedProjects = makeUnfeaturedProjectList(openSourceProjects);
   return (
     <main>
       <h1>My Open Source Contributions...</h1>
-      <section>
+      {featuredProjects.length > 0 && <section>
           <h2>Featured...</h2>
-          <List dataList={dataForList} ListedComponent={OpenSourceProject}/>
-      </section>
-      <section>
-          <h2>More...</h2>
-      </section>
+          <List dataList={featuredProjects.map(project => ({project, contributorUsername: 'JamesGJ5'}))} ListedComponent={OpenSourceProject}/>
+      </section>}
+      {unfeaturedProjects.length > 0 && <section>
+          <h2>{featuredProjects.length ? 'More' : 'Featured'}...</h2>
+          <List dataList={unfeaturedProjects.map(project => ({project, contributorUsername: 'JamesGJ5'}))} ListedComponent={OpenSourceProject}/>
+      </section>}
     </main>
   );
 }
