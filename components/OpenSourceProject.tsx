afa88e71ebb6ebe5f660945c79dc01d79b8bd188
James Graca-Jones <jamesgracajones@gmail.com>
2024-01-10 18:51:53 +0000

    Add TODO

--- components/OpenSourceProject.tsx
+++ components/OpenSourceProject.tsx
@@ -5,6 +5,7 @@ function makePullRequestsURL(repoName: string, contributorUsername: string): str
     return `https://github.com/${repoName}/pulls?q=is:pr+author:${contributorUsername}`;
 }
 
+// TODO: consider just using Project components for open source projects and having contributorUsername as a parameter of Project components.
 export default function OpenSourceProject({ project, contributorUsername }: OpenSourceProjectProps) {
     const pullRequestsURL = makePullRequestsURL(project.repoName, contributorUsername);
     return (
