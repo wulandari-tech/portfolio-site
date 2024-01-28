5fbaa4379559ce07fc41d868f69abea85da16a53
James Graca-Jones <jamesgracajones@gmail.com>
2024-01-28 16:49:51 +0000

    Add TODO

--- app/projects/error.tsx
+++ app/projects/error.tsx
@@ -3,6 +3,7 @@
 export default function ErrorBoundary() {
     return (
         <p>
+            {/* TODO: just put the link in this text rather than directing the user to it */}
             Whoops! It looks like there may have been an issue loading my projects but follow the link in the navigation bar to my GitHub profile to discover more.
         </p>
     );
