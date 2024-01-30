4ca33fc854bb01debdab280f4366ab80b56dd5a3
James Graca-Jones <jamesgracajones@gmail.com>
2024-01-30 22:43:54 +0000

    Refactor contribution element logic

--- components/OpenSourceProject.tsx
+++ components/OpenSourceProject.tsx
@@ -1,12 +1,18 @@
+import React from "react";
 import ProjectComponent from "./Project";
 import { OpenSourceProjectProps } from "@/types";
 
+function makeContributionElement(type: string, url: string): React.ReactElement {
+    return <li><p>{type}: <a aria-label={`Link to ${type} (opens in new tab)`} href={url} target='_blank'>{url}</a></p></li>;
+}
+
 export default function OpenSourceProject({ openSourceProject }: OpenSourceProjectProps) {
+    const contributions = openSourceProject.contributions;
     return (
         <>
             <ProjectComponent project={openSourceProject}/>
             <ul>
-                {Object.entries(openSourceProject.contributions).map(([type, url]) => <li><p>{type}: <a aria-label='Link to contributions (opens in new tab)' href={url} target='_blank'>{url}</a></p></li>)}
+                {Object.entries(contributions).map(([type, url]) => makeContributionElement(type, url))}
             </ul>
         </>
     );
