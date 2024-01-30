99750797d5b51ae6d66f2339c59fe5a4b7c23d85
James Graca-Jones <jamesgracajones@gmail.com>
2024-01-30 22:19:31 +0000

    Remove comments

--- data/openSourceProjects.ts
+++ data/openSourceProjects.ts
@@ -6,8 +6,6 @@ const openSourceProjects: OpenSourceProject[] = [
         repoDescripion: 'The open curriculum for learning web development.',
         isFeatured: true,
         languagesUsed: ['Markdown'],
-        // // TODO: consider dynamically loading the date of last modified from GitHub or ranking open source projects with another metric (or even a static one)
-        // lastModified: new Date(2023, 5, 24, 21, 37, 0),
         deploymentURL: 'https://www.theodinproject.com/',
         contributions: {
             'Pull requests': 'https://github.com/TheOdinProject/curriculum/pulls?q=is:pr+author:JamesGJ5',
