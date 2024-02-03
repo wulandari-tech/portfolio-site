5d6ce0902d051baa8af38ba7f39a7cf69cd1e18e
James Graca-Jones <jamesgracajones@gmail.com>
2024-02-03 22:32:13 +0000

    List Certifications

--- app/certifications/page.tsx
+++ app/certifications/page.tsx
@@ -1,8 +1,9 @@
 import Certification from "@/components/Certification";
+import List from "@/components/List";
 import { certifications } from "@/data/certifications";
 
 export default function CertificationsPage() {
     return (
-        <></>
+        <List dataList={certifications.map(certification => ({certification}))} ListedComponent={Certification}/>
     );
 }
