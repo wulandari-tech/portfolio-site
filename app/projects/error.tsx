d2a4f4cd7f640b072327fb7d38fcbd1ed051e1be
James Graca-Jones <jamesgracajones@gmail.com>
2024-02-03 21:39:35 +0000

    Improve error page message

--- app/projects/error.tsx
+++ app/projects/error.tsx
@@ -4,7 +4,7 @@ export default function ErrorBoundary() {
     return (
         <p>
             {/* TODO: just put the link in this text rather than directing the user to it */}
-            Whoops! It looks like there may have been an issue loading my projects but follow the link in the navigation bar to my GitHub profile to discover more.
+            Whoops! It looks like there may have been an issue loading my projects but feel free to see my <a aria-label='Link to GitHub repos (opens in new tab)' href='https://github.com/JamesGJ5?tab=repositories' target='_blank'>GitHub repos</a> or contact me via <a aria-label='Link to LinkedIn profile (opens in new tab)' href='https://www.linkedin.com/in/james-graca-jones' target='_blank'>LinkedIn</a> to learn more! :)
         </p>
     );
 }
