da7b525b5fe3f12dc8cec3311243ef52e8d5a090
James Graca-Jones <jamesgracajones@gmail.com>
2024-02-05 21:35:32 +0000

    Use list index for key

--- components/List.tsx
+++ components/List.tsx
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 export default function List<T extends object>({ dataList, ListedComponent }: ListProps<T>) {
     return (
         <>
-            {dataList.map(data => <li key={uuidv4()}>
+            {dataList.map((data, index) => <li key={index}>
                 <ListedComponent {...data}/>
             </li>)}
         </>
