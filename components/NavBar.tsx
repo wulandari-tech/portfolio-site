681184142164f650c62980b9fa0b57a716dd01c6
James Graca-Jones <jamesgracajones@gmail.com>
2024-02-11 17:08:50 +0000

    Give keys to nav bar li's

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,26 +5,26 @@ export default function NavBar() {
     return (
         <nav>
             <ul>
-                <li>
+                <li key={0}>
                     <Link href={'/'}>Index</Link>
                 </li>
-                <li>
+                <li key={1}>
                     <Link href={'/projects'}>Projects</Link>
                 </li>
-                <li>
+                <li key={2}>
                     <Link href={'/open-source'}>Open Source</Link>
                 </li>
-                <li>
+                <li key={3}>
                     <Link href={'/certifications'}>Certifications</Link>
                 </li>
             </ul>
             <ul>
-                <li>
+                <li key={0}>
                     <a aria-label='Link to GitHub profile (opens in new tab)' href='https://github.com/JamesGJ5' target='_blank'>
                         <Image src='/logos/github-mark.svg' alt='' width={50/96*98} height={50} priority/>
                     </a>
                 </li>
-                <li>
+                <li key={1}>
                     <a aria-label='Link to LinkedIn profile (opens in new tab)' href='https://www.linkedin.com/in/james-graca-jones' target='_blank'>
                         <Image src='/logos/LI-In-Bug.png' alt='' width={50/540*635} height={50} priority/>
                     </a>
