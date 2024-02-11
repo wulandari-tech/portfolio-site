29ab0b6a97c556de90d424e350a5cec16effae4c
James Graca-Jones <jamesgracajones@gmail.com>
2024-02-11 18:32:58 +0000

    Give keys to contribution lists

--- components/OpenSourceProject.tsx
+++ components/OpenSourceProject.tsx
@@ -3,8 +3,8 @@ import Project from "@/components/Project";
 import Skills from "@/components/Skills";
 import { OpenSourceProjectProps } from "@/types";
 
-function makeContributionElement(type: string, url: string): React.ReactElement {
-    return <li><p>{type}: <a aria-label={`Link to ${type} (opens in new tab)`} href={url} target='_blank'>{url}</a></p></li>;
+function makeContributionElement(type: string, url: string, index: number): React.ReactElement {
+    return <li key={index}><p>{type}: <a aria-label={`Link to ${type} (opens in new tab)`} href={url} target='_blank'>{url}</a></p></li>;
 }
 
 export default function OpenSourceProject({ openSourceProject }: OpenSourceProjectProps) {
@@ -14,7 +14,7 @@ export default function OpenSourceProject({ openSourceProject }: OpenSourceProje
             <Project project={project}/>
             <Skills skills={languagesUsed}/>
             <ul>
-                {Object.entries(contributions).map(([type, url]) => makeContributionElement(type, url))}
+                {Object.entries(contributions).map(([type, url], index) => makeContributionElement(type, url, index))}
             </ul>
         </>
     );
