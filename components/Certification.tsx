54f1101ffadd0558a7d1ed82b4603185b476aa27
James Graca-Jones <jamesgracajones@gmail.com>
2024-02-19 19:48:48 +0000

    Remove credential ID

--- components/Certification.tsx
+++ components/Certification.tsx
@@ -3,13 +3,12 @@ import { format } from "date-fns";
 import Image from "next/image";
 
 export default function Certification({ certification }: CertificationProps) {
-    const { name, issuer, issueDate, credentialUrl, credentialId, certificationImageSrc } = certification;
+    const { name, issuer, issueDate, credentialUrl, certificationImageSrc } = certification;
     return (
         <>
             <h2>{name}</h2>
             <p>Issued by {issuer}</p>
             <p>Issued on {format(issueDate, 'yyyy-MM-dd')}</p>
-            <p>Credential ID: {credentialId}</p>
             <p>Click <a aria-label='Link to certification (opens in new tab)' href={credentialUrl} target='_blank'>here</a> to see the certification...</p>
             {certificationImageSrc && <Image src={certificationImageSrc} alt={`Logo of Certification ${name} from ${issuer}`} width={50/96*98} height={50} />}
         </>
