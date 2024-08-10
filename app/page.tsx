856f76c2d83fe700a6222545f1c6589143912d70
James Graca-Jones <jamesgracajones@gmail.com>
2024-08-10 14:23:14 +0100

    Update further skills

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function IndexPage() {
       <section>
         <h2>Further Knowledge and Skills...</h2>
         <Skills skills={
-          ['AWS Identity and Access Management', 'AWS Lambda', 'Amazon DynamoDB', 'Amazon API Gateway', 'AWS Amplify', 'Docker', 'Material-UI', 'Web Scraping (Puppeteer)', 'Data Structures & Algorithms', 'Operating Systems', 'Networking', 'System Design']
+          ['AWS Identity and Access Management', 'AWS Lambda', 'Amazon DynamoDB', 'Amazon API Gateway', 'AWS Amplify', 'Docker', 'Material-UI', 'Web Scraping (Puppeteer)', 'Data Structures & Algorithms']
         }/>
       </section>
     </main>
